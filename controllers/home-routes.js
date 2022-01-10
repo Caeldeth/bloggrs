609977af6e6f5b38dbe5469d2243a91a69ff94d7
Caeldeth <sabrael@hybrasyl.com>
2022-01-10 13:43:32 -0600

    finish setting up server.js for initial testing

---
+++ controllers/home-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+// fetch all posts for homepage
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['pst_id', 'pst_title', 'pst_content', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['cmt_id', 'cmt_text', 'cmt_pst_id', 'cmt_usr_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['usr_name']
+        }
+      },
+      {
+        model: User,
+        attributes: ['usr_name']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single post
+router.get('/post/:pst_id', (req, res) => {
+  Post.findOne({
+    where: {
+      pst_id: req.params.pst_id
+    },
+    attributes: ['pst_id', 'pst_title', 'pst_content', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['cmt_id', 'cmt_text', 'cmt_pst_id', 'cmt_usr_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['usr_name']
+        }
+      },
+      {
+        model: User,
+        attributes: ['usr_name']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,3 +15,30 @@ const sequelize = require("./config/connection");
 const session = require("express-session");
 // add sequelize session store
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+//set up express session
+const sess = {
+    secret: "above meat chain",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+//define server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+app.use("/", controller);
+
+//start server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});
