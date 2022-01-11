284bc1a2cdc88bb3335979fe5413574bf9dde8ca
Caeldeth <sabrael@hybrasyl.com>
2022-01-11 14:33:36 -0600

    borrow date formatter from previous lesson

---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // helper to format a date
+    format_date: date => {
+      return `${new Date(date).getMonth() +1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`
+    }
+  }
